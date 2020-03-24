9683c643a0ca6a0ecc4a0bd646acfcfaa52782fb
sunil poojari <sunil.poojari217@siesgst.ac.in>
2020-03-24 18:02:26 +0530

    new chnages

---
+++ main.js
@@ -0,0 +1,60 @@
+const navSlide = () =>{
+    const burger = document.querySelector(".burger");
+    const nav = document.querySelector(".nav-links");
+    const navLinks = document.querySelectorAll('.nav-links li')
+
+    burger.addEventListener('click',()=>{
+        nav.classList.toggle('nav-active');
+        //animate links
+        navLinks.forEach((link,index)=>{
+            if(link.style.animation){
+                link.style.animation = '';
+            }else{
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index/7 + 0.5}s`
+            }
+        })
+        
+        
+        //buger animation
+        burger.classList.toggle('toggle')
+    })
+
+    navLinks.forEach((link)=>{
+        link.addEventListener('click',()=>{
+            nav.classList.toggle('nav-active');
+            navLinks.forEach((link,index)=>{
+                if(link.style.animation){
+                    link.style.animation = '';
+                }else{
+                    link.style.animation = `navLinkFade 0.5s ease forwards ${index/7 + 0.5}s`
+                }
+            })
+            burger.classList.toggle('toggle')
+
+    })
+    })
+
+    
+}
+
+function toggle_class(){
+    // console.log(document.querySelector(".burger").classList.length,"hiii")
+    const navLinks = document.querySelectorAll('.nav-links li')
+    if(document.querySelector(".burger").classList.length == 2){
+        document.querySelector(".nav-links").classList.toggle('nav-active');
+        document.querySelector(".burger").classList.toggle('toggle')
+        navLinks.forEach((link,index)=>{
+            if(link.style.animation){
+                link.style.animation = '';
+            }else{
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index/7 + 0.5}s`
+            }
+        })
+    }
+}
+// document.querySelector("nav").addEventListener('blur',toggle_class,true)
+// document.querySelector("#container").addEventListener('click',toggle_class,true)
+// document.querySelector("#container").addEventListener('scroll',toggle_class)
+window.onscroll = function() {this.toggle_class()};
+    
+navSlide();
